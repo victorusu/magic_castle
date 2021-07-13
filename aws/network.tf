b23021bf7205ae3c7b62d55e6f0ceb0140e7a42c
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-07-13 10:49:53 -0400

    Add subnet for efa enabled instances

--- aws/network.tf
+++ aws/network.tf
@@ -35,6 +35,17 @@ resource "aws_subnet" "subnet" {
   }
 }
 
+resource "aws_subnet" "efa_subnet" {
+  vpc_id     = aws_vpc.network.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = local.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.cluster_name}-efa-subnet"
+  }
+}
+
 resource "aws_security_group" "allow_out_any" {
   name   = "allow_out_any"
   vpc_id = aws_vpc.network.id
@@ -97,7 +108,7 @@ resource "aws_security_group" "allow_any_inside_vpc" {
 
 resource "aws_network_interface" "nic" {
   for_each        = module.design.instances
-  subnet_id       = aws_subnet.subnet.id
+  subnet_id       = contains(each.value["tags"], "efa") ? aws_subnet.efa_subnet.id : aws_subnet.subnet.id
   interface_type  = contains(each.value["tags"], "efa") ? "efa" : null
 
   security_groups = concat(
